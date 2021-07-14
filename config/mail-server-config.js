baa62d8c69e8787761d3779d292fdbf6e7201d11
dteso <dtesodev@gmail.com>
2021-07-14 21:14:34 +0200

    nodemailer basic implementation

---
+++ config/mail-server-config.js
@@ -0,0 +1,7 @@
+exports.mailServerConfig = {
+    service: 'Gmail',
+    auth: {
+        user: 'digitalislandsp@gmail.com',
+        pass: '@3lp4rr07'
+    }
+}
--- controllers/dispatcher.controller.js
+++ controllers/dispatcher.controller.js
@@ -1,11 +1,13 @@
 
 const { BaseController } = require('./base.controller');
+const { sendCustomMail } = require('../helpers/mail');
 
 class DispatcherController extends BaseController {
 
     sendMail = async (req, res) => {
         try{
-            console.log("Post to send mail OK!!!!")
+            sendCustomMail('MY SYSTEM', 'dtesodev@gmail.com', 'INTRUSIÓN DETECTADA', 'Se ha detectado un salto de alarma, por favor, revise su App');
+            console.log("Enviado Email de aviso");
             res.json({
                 ok: true,
                 msg: 'Received request to post Mail'
